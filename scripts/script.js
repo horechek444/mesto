72876f5db440f69e34e54c7a0d8d551be472cd55
Tatiana Eskina <horechek444@gmail.com>
2020-06-11 14:28:56 +0300

    feat: добавить код JS, исправить мелкие недостатки

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,42 @@
-// Отсутствуют ошибки в консоли.
-// При нажатии на кнопку “Edit” появляется всплывающее окно. Информация из профиля пользователя загружается в соответствующие поля.
-// При открытом попапе нажатие на клавишу “Enter” или кнопку «Сохранить» изменяет на странице информацию о пользователе.
-// При нажатии на крестик попап закрывается.
-// DOM-элементы, к которым есть обращение в скрипте, вынесены в константы.
-// Переменные объявлены через let.
-// Отсутствуют неиспользуемые участки кода.
+const editButton = document.querySelector('.profile__button');
+const popupWindow = document.querySelector('.popup');
+const popupClose = popupWindow.querySelector('.popup__close');
+const formElement = popupWindow.querySelector('.popup__form');
+const popupSubmit = formElement.querySelector('.popup__submit');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_about');
+const nameElement = document.querySelector('.profile__title');
+const jobElement = document.querySelector('.profile__subtitle');
 
-const editButton = document.querySelector('profile__button');
-const popup = document.querySelector('popup');
-const popupClose = popup.querySelector('popup__close');
-const formElement = popup.querySelector('popup__form');
-const popupSubmit = formElement.querySelector('popup__submit');
-const nameInput = formElement.querySelector('popup__input_type_name');
-const jobInput = formElement.querySelector('popup__input_type_about');
-const nameElement = document.querySelector('profile__title');
-const jobElement = document.querySelector('profile__subtitle');
-
-function formSubmitHandler(evt) {
+function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    // Получите значение полей из свойства value
-    let nameValue = nameInput.getAttribute('value');
-    let jobValue = jobInput.getAttribute('value');
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
-    nameValue.textContent = nameElement;
-    jobValue.textContent = jobElement;
+
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+
+    nameElement.textContent = nameValue;
+    jobElement.textContent = jobValue;
 }
 
-popupSubmit.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
 
-function formCloser() {
-    
+
+function popupOpener () {
+
     editButton.addEventListener('click',function (evt) {
         evt.preventDefault();
-        popup.classList.add(popup__opened);
-    });
-    
-    popupClose.addEventListener('click',function (evt) {
-        evt.preventDefault();
-        popup.classList.remove(popup__opened);
+        popupWindow.classList.add('popup_opened');
     });
 }
+
+popupOpener();
+
+function popupCloser () {
+
+    popupClose.addEventListener('click',function (evt) {
+        evt.preventDefault();
+        popupWindow.classList.remove('popup_opened');
+    });
+}
+
+popupCloser();
