445e74ea5c5213c4989a2630d83ea613cc97acd8
Tatiana Eskina <horechek444@gmail.com>
2020-06-13 14:13:44 +0300

    fix: исправить ошибки по результатам первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,33 @@ const jobInput = formElement.querySelector('.popup__input_type_about');
 const nameElement = document.querySelector('.profile__title');
 const jobElement = document.querySelector('.profile__subtitle');
 
-function formSubmitHandler (event) {
+// В целом JS код работает не совсем так, как должен по заданию.
+// По идее здесь должно быть три функции - первая открывает попап 
+//и копирует данные со странички в инпуты попапа, вторая закрывает попап, 
+//а третья копирует данные из инпутов попапа в страничку. А чтобы закрыть 
+// попап можно в конце третьей функции вызвать вторую. 
+// Как "можно лучше" с помощью if первую и вторую функцию можно объединить в одну.
+
+function popupToggle() {
+    
+    if (popupWindow.classList.contains('popup_opened')) {
+        popupWindow.classList.remove('popup_opened');
+    } else {
+        popupWindow.classList.add('popup_opened');
+
+        nameInput.value = nameElement.textContent;
+        jobInput.value = jobElement.textContent;
+    }
+}
+
+function formSubmitHandler(event) {
     event.preventDefault(); 
-
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
-
-    nameElement.textContent = nameValue;
-    jobElement.textContent = jobValue;
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-function popupToggle(event) {
-    popupWindow.classList.toggle('popup_opened');
-}
-
-function closeOutOfPopup(event) {
-    if (event.target !== event.currentTarget) {
-        return
-    } popupToggle();
+    
+    nameElement.textContent = nameInput.value;
+    jobElement.textContent = jobInput.value;
+    popupToggle();
 }
 
 editButton.addEventListener('click', popupToggle);
 popupClose.addEventListener('click', popupToggle);
-popupWindow.addEventListener('click', closeOutOfPopup);
+formElement.addEventListener('submit', formSubmitHandler);
