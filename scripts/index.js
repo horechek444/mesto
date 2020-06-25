5e61f606e24bc6174b0bc3f2d81dd6a78dfed4e6
Tatiana Eskina <horechek444@gmail.com>
2020-06-25 17:39:28 +0300

    feat: реализовать добавление на страницу элементов с помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
-let editButton = document.querySelector('.profile__button');
-let editPopup = document.querySelector('.edit');
-let editClose = editPopup.querySelector('.edit__close');
-let editForm = editPopup.querySelector('.edit__form');
-let editSubmit = editForm.querySelector('.edit__submit');
-let nameInput = editForm.querySelector('.popup__input_type_name');
-let jobInput = editForm.querySelector('.popup__input_type_about');
-let nameElement = document.querySelector('.profile__title');
-let jobElement = document.querySelector('.profile__subtitle');
+const editButton = document.querySelector('.profile__button');
+const editPopup = document.querySelector('.edit');
+const editClose = editPopup.querySelector('.edit__close');
+const editForm = editPopup.querySelector('.edit__form');
+const editSubmit = editForm.querySelector('.edit__submit');
+const nameInput = editForm.querySelector('.popup__input_type_name');
+const jobInput = editForm.querySelector('.popup__input_type_about');
+const nameElement = document.querySelector('.profile__title');
+const jobElement = document.querySelector('.profile__subtitle');
 
-let addButton = document.querySelector('.button_add');
-let addPopup = document.querySelector('.add');
-let addClose = addPopup.querySelector('.add__close');
-let addForm = addPopup.querySelector('.add__form');
-let addSubmit = addForm.querySelector('.add__submit');
-let titleInput = addForm.querySelector('.popup__input_type_title');
-let imageLinkInput = addForm.querySelector('.popup__input_type_link');
+const addButton = document.querySelector('.button_add');
+const addPopup = document.querySelector('.add');
+const addClose = addPopup.querySelector('.add__close');
+const addForm = addPopup.querySelector('.add__form');
+const addSubmit = addForm.querySelector('.add__submit');
+const titleInput = addForm.querySelector('.popup__input_type_title');
+const imageLinkInput = addForm.querySelector('.popup__input_type_link');
 
-let pictureTemplate = document.querySelector('#picture').content;
-let pictures = document.querySelector('.pictures__list');
+const pictureTemplate = document.querySelector('#picture').content;
+const pictures = document.querySelector('.pictures__list');
 
 // Открытие-закрытие попапов
 function popupToggle(popup) {
@@ -48,19 +48,39 @@ editForm.addEventListener('submit', formSubmitHandler);
 addButton.addEventListener('click', popupToggle(addPopup));
 addClose.addEventListener('click', popupToggle(addPopup));
 
-function elementMaker() {
-    let pictureElements = [];
-    for (let i = 0; i < 6; i++) {
-        pictureElements[i] = pictureTemplate.cloneNode(true);
-        
-    } return pictureElements;
+// Добавление первых 6 элементов на страницу
+const initialCards = [
+    {
+        name: 'Калининград',
+        link: './images/kaliningrad.jpg'
+    },
+    {
+        name: 'Саха',
+        link: './images/sakha.jpg'
+    },
+    {
+        name: 'Владивосток',
+        link: './images/vladivostok.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: './images/kamchatka.jpg'
+    },
+    {
+        name: 'Сибирь',
+        link: './images/siberia.jpg'
+    },
+    {
+        name: 'Клин',
+        link: './images/klin.jpg'
+    }
+];
+
+function elementMaker(item) {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.pictures__image').src = item.link;
+    pictureElement.querySelector('.pictures__title').textContent = item.name;
+    pictures.append(pictureElement);
 }
-console.log(elementMaker());
 
-let pictureElement_1 = pictureTemplate.cloneNode(true);
-
-pictureElement_1.querySelector('.pictures__image').src = './images/kaliningrad.jpg';
-pictureElement_1.querySelector('.pictures__image').alt = "Калининград, двор-колодец";
-pictureElement_1.querySelector('.pictures__title').textContent = 'Калининград';
-
-pictures.append(pictureElement_1);
+initialCards.forEach(elementMaker);
