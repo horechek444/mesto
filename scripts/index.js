55f3bf1014ada4754bdbc96bad6405d2c2c9a917
Tatiana Eskina <horechek444@gmail.com>
2020-06-27 19:28:22 +0300

    fix: исправить замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ const editButton = document.querySelector('.profile__button');
 const editPopup = document.querySelector('.edit');
 const editPopupClose = editPopup.querySelector('.edit__close');
 const editForm = editPopup.querySelector('.edit__form');
-const editSubmit = editForm.querySelector('.edit__submit');
 const nameInput = editForm.querySelector('.popup__input_type_name');
 const jobInput = editForm.querySelector('.popup__input_type_about');
 const nameElement = document.querySelector('.profile__title');
@@ -12,7 +11,6 @@ const addButton = document.querySelector('.button_add');
 const addPopup = document.querySelector('.add');
 const addPopupClose = addPopup.querySelector('.add__close');
 const addForm = addPopup.querySelector('.add__form');
-const addSubmit = addForm.querySelector('.add__submit');
 const titleInput = addForm.querySelector('.popup__input_type_title');
 const linkInput = addForm.querySelector('.popup__input_type_link');
 
@@ -23,49 +21,6 @@ const picturePopupCaption = picturePopup.querySelector('.popup__caption');
 
 const picturesTemplateElement = document.querySelector('.pictures-template');
 const picturesListElement = document.querySelector('.pictures__list');
-
-function popupToggle(popup) {
-    return function () {
-        popup.classList.toggle('popup_opened');
-    } 
-}
-
-function editFormSubmitHandler(event) {
-    event.preventDefault(); 
-    
-    nameElement.textContent = nameInput.value;
-    jobElement.textContent = jobInput.value;
-    popupToggle(editPopup)();
-}
-
-function addFormSubmitHandler(event) {
-    event.preventDefault();
-
-    const name = titleInput.value;
-    const link = linkInput.value;
-
-    titleInput.value = '';
-    linkInput.value = '';
-
-    addPicturesElement(name, link);
-    popupToggle(addPopup)();
-}
-
-editButton.addEventListener('click', function() {
-    popupToggle(editPopup)();
-
-    nameInput.value = nameElement.textContent;
-    jobInput.value = jobElement.textContent;
-});
-
-editPopupClose.addEventListener('click', popupToggle(editPopup));
-editForm.addEventListener('submit', editFormSubmitHandler);
-
-addButton.addEventListener('click', popupToggle(addPopup));
-addPopupClose.addEventListener('click', popupToggle(addPopup));
-addForm.addEventListener('submit', addFormSubmitHandler);
-
-picturePopupClose.addEventListener('click', popupToggle(picturePopup));
 
 const initialPicturesElements = [
     {
@@ -94,21 +49,16 @@ const initialPicturesElements = [
     }
 ];
 
-function addPicturesElement(name, link) {
-    const picturesElement = picturesTemplateElement.content.cloneNode(true);
-
-    picturesElement.querySelector('.pictures__title').textContent = name;
-    picturesElement.querySelector('.pictures__image').src = link;
-    picturesElement.querySelector('.pictures__image').alt = name;
-    addPicturesElementListeners(picturesElement);
-
-    picturesListElement.prepend(picturesElement);
+function popupToggle(popup) {
+    popup.classList.toggle('popup_opened');
 }
 
-function addPicturesElementListeners(picturesElement) {
-    picturesElement.querySelector('.pictures__delete').addEventListener('click', deletePicturesElement);
-    picturesElement.querySelector('.pictures__like').addEventListener('click', likePicturesElement);
-    picturesElement.querySelector('.pictures__image').addEventListener('click', showPopupPicturesElement);
+function editFormSubmitHandler(event) {
+    event.preventDefault(); 
+    
+    nameElement.textContent = nameInput.value;
+    jobElement.textContent = jobInput.value;
+    popupToggle(editPopup);
 }
 
 function deletePicturesElement(event) {
@@ -120,15 +70,61 @@ function likePicturesElement(event) {
     event.target.classList.toggle('pictures__like_active');
 }
 
-initialPicturesElements.forEach(item => {
-    addPicturesElement(item.name, item.link);
-});
-
 function showPopupPicturesElement(event) {
     const picturesElement = event.target.closest('.pictures__item');
 
     picturePopupCaption.textContent = picturesElement.querySelector('.pictures__title').textContent;
     picturePopupImage.src = picturesElement.querySelector('.pictures__image').src;
     picturePopupCaption.textContent = picturesElement.querySelector('.pictures__image').alt;
-    picturePopup.classList.add('popup_opened');
+    popupToggle(picturePopup);
 }
+
+function addPicturesElementListeners(picturesElement) {
+    picturesElement.querySelector('.pictures__delete').addEventListener('click', deletePicturesElement);
+    picturesElement.querySelector('.pictures__like').addEventListener('click', likePicturesElement);
+    picturesElement.querySelector('.pictures__image').addEventListener('click', showPopupPicturesElement);
+}
+
+function addPicturesElement(name, link) {
+    const picturesElement = picturesTemplateElement.content.cloneNode(true);
+
+    picturesElement.querySelector('.pictures__title').textContent = name;
+    picturesElement.querySelector('.pictures__image').src = link;
+    picturesElement.querySelector('.pictures__image').alt = name;
+    addPicturesElementListeners(picturesElement);
+
+    picturesListElement.prepend(picturesElement);
+}
+
+function addFormSubmitHandler(event) {
+    event.preventDefault();
+
+    const name = titleInput.value;
+    const link = linkInput.value;
+
+    titleInput.value = '';
+    linkInput.value = '';
+
+    addPicturesElement(name, link);
+    popupToggle(addPopup);
+}
+
+editButton.addEventListener('click', function() {
+    nameInput.value = nameElement.textContent;
+    jobInput.value = jobElement.textContent;
+
+    popupToggle(editPopup);
+});
+
+editPopupClose.addEventListener('click', () => popupToggle(editPopup));
+editForm.addEventListener('submit', editFormSubmitHandler);
+
+addButton.addEventListener('click', () => popupToggle(addPopup));
+addPopupClose.addEventListener('click', () => popupToggle(addPopup));
+addForm.addEventListener('submit', addFormSubmitHandler);
+
+picturePopupClose.addEventListener('click', () => popupToggle(picturePopup));
+
+initialPicturesElements.forEach(item => {
+    addPicturesElement(item.name, item.link);
+});
